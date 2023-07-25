bf9618fc8b8926b22a5f0c24c96d75a408d3a3a7
AbdullahUguz <abdullahuguz08@gmail.com>
2023-07-25 11:48:36 +0300

    Initial commit

---
+++ src/main/java/com/kafein/dayOffScheduleBackend/DayOffScheduleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kafein.dayOffScheduleBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayOffScheduleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayOffScheduleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafein/dayOffScheduleBackend/DayOffScheduleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafein.dayOffScheduleBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayOffScheduleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
